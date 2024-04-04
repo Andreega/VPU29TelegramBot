a30a06693122e6bd467a394b089b1786b6f8c473
Andreega <andriyvoitvych@gmail.com>
2024-04-04 14:11:07 +0300

    added telegram bot

--- main.py
+++ main.py
@@ -1,29 +1,46 @@
 import os
-import logging
-
-from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from telegram import Update, KeyboardButton, ReplyKeyboardMarkup
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler, filters
 from dotenv import load_dotenv
-
+import logging
 load_dotenv()
-
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
-
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO
+    level=logging.DEBUG
+
 )
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    keyboard = [
+        [KeyboardButton('Hello'), KeyboardButton('World')],
+        [KeyboardButton('Goodbye')]
+    ]
+    reply_markup = ReplyKeyboardMarkup(keyboard)
+    await update.message.reply_text(f'Start {update.effective_user.first_name}', reply_markup=reply_markup)
+async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(f'Здоров {update.effective_user.first_name}')
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    massage = update.message.text.lower()
+    if 'привіт' in massage:
+        reply_text = f"Доброго дня {update.effective_user.first_name}"
+    elif massage == 'допобачення':
+        last_name = update.effective_user.last_name
+        if last_name is None:
+            reply_text = f"Допобачення {update.effective_user.first_name}"
+        else:
+            reply_text = f"Допобачення {update.effective_user.first_name} {last_name}"
+    else:
+        reply_text = 'Я тебе не зрозумів.'
 
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
+    await update.message.reply_text(reply_text)
+app = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
+
+app.add_handler(CommandHandler("Hello", hello))
+app.add_handler(CommandHandler("Start", start))
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
 
 
-if __name__ == '__main__':
-    application = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
-
-    start_handler = CommandHandler('start', start)
-    application.add_handler(start_handler)
-
-    application.run_polling()
+app.run_polling()
